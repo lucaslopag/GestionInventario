540e576dd1456ade183a71b6e316b6cc0ff2b367
Ángel López <caza.angel.lopez@gmail.com>
2026-05-12 11:25:16 +0200

    ms-audit sin probar

---
+++ ms-audit/src/main/java/com/tfg/ms_audit/repository/LogAuditoriaRepository.java
@@ -0,0 +1,16 @@
+package com.tfg.ms_audit.repository;
+
+import com.tfg.ms_audit.model.LogAuditoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface LogAuditoriaRepository extends JpaRepository<LogAuditoria, Long> {
+
+    List<LogAuditoria> findByProductoIdOrderByFechaDesc(Long productoId);
+
+    List<LogAuditoria> findByUsuarioEmailOrderByFechaDesc(String usuarioEmail);
+
+    List<LogAuditoria> findAllByOrderByFechaDesc();
+}
---
+++ ms-audit/src/test/java/com/tfg/ms_audit/MsAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.ms_audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
